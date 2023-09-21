35a37c7538d6abec78085ffe243e8f7c7ee6e54b
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-09-21 10:52:10 +0530

    lets try next time

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import Sidebar from "./components/Sidebar";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const themeDark = createTheme({
+  palette: {
+    background: {
+      default: "#1F2129",
+    },
+    text: {
+      primary: "#FEFEFE",
+    },
+  },
+  components: {
+    MuiDrawer: {
+      styleOverrides: {
+        paper: {
+          backgroundColor: "#10182800",
+          color: "#FEFEFE",
+        },
+      },
+    },
+  },
+});
+
 function App() {
-  return <></>;
+  return (
+    <ThemeProvider theme={themeDark}>
+      <Sidebar />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,158 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MailIcon from "@mui/icons-material/Mail";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import SearchIcon from "@mui/icons-material/Search";
+
+const drawerWidth = 240;
+
+export default function Sidebar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <img
+        src="Logo.png"
+        alt="Logo"
+        style={{
+          padding: "10px",
+
+          width: "218px",
+          height: "auto",
+        }}
+      />
+
+      <List>
+        {[
+          { text: "Dashboard", icon: "bar-chart-2.png" },
+          { text: "Projects", icon: "3-layers.png" },
+          { text: "Staffing", icon: "users.png" },
+          { text: "Profile", icon: "solar_chat-line-broken.png" },
+        ].map((item) => (
+          <ListItem key={item.text} disablePadding>
+            <ListItemButton>
+              {/* Replace the InboxIcon with a custom image */}
+              <img
+                src={item.icon}
+                alt={item.text}
+                style={{
+                  width: "24px", // Adjust the width and height as needed
+                  height: "24px",
+                  marginRight: "16px", // Add margin to separate the image from text
+                }}
+              />
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          backgroundColor: "#1F2129",
+          color: "#FEFEFE",
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={handleDrawerToggle}
+          sx={{ mr: 2, display: { sm: "none" } }}
+        >
+          <MenuIcon />
+        </IconButton>
+
+        <TextField
+          id="search"
+          // Add InputAdornment to display the search icon on the left
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
+      >
+        <Toolbar />
+      </Box>
+    </Box>
+  );
+}
